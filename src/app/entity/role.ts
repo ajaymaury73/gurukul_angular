aecf7d0ec6358389bbc4b2fc9773d8f7c0f884f4
AJAY KUMAR MAURYA <ajay.km@heraizen.com>
2025-03-18 13:54:40 +0530

    role change

--- src/app/entity/role.ts
+++ src/app/entity/role.ts
@@ -1,10 +1,10 @@
 export enum Role {
-    ADMIN = 'Admin',
-    STUDENT ='Student',
-    PRINCIPAL = 'Principal',
-    COLLEGE_ADMIN='College Admin',
-    TEACHER = 'Teacher',
-    EXAM_COORDINATOR = 'ExamCoordinator',
-    EXAM_CONTROLLER = 'ExamController'
+    ADMIN = 'ADMIN',
+    STUDENT ='STUDENT',
+    PRINCIPAL = 'PRINCIPAL',
+    COLLEGE_ADMIN='COLLEGE_ADMIN',
+    TEACHER = 'TEACHER',
+    EXAM_COORDINATOR = 'EXAM_COORDINATOR',
+    EXAM_CONTROLLER = 'EXAM_CONTROLLER'
   }
   
